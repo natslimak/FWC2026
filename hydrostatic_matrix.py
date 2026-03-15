953cb93f949e5d0a4527f7fa9cdf1f703cdb0ecb
Francesco Richard <francesco.richard.02@gmail.com>
2026-03-15 10:57:11 +0100

    add comments to code

--- hydrostatic_matrix.py
+++ hydrostatic_matrix.py
@@ -5,8 +5,6 @@ to better understand the imput mesurement you can refer to the README file'''
 
 #imports
 import numpy as np
-from scipy.linalg import eig
-import sys
 
 ## ambient parameters
 
@@ -59,21 +57,21 @@ zmoor = -70         # Mooring fairlead [m]
 
 m_back =  2 * (mt + mn + mr + m_bb + m_bf)   # Back side mass [kg] 
 
-m_front = m_fb + m_ff
+m_front = m_fb + m_ff                        # Front side mass [kg] 
 
-m_tot = m_back + m_front
+m_tot = m_back + m_front                     # Total mass
 
-A_back = np.pi * D_b**2 / 4
+A_back = np.pi * D_b**2 / 4                  # Sectional area back cylinders [m^2]
 
-A_front = np.pi * D_f**2 / 4
+A_front = np.pi * D_f**2 / 4                 # Sectional area front cylinder [m^2]
 
-A_total = 2 * A_back + A_front
+A_total = 2 * A_back + A_front               # total sectional area [m^2]
 
-draft = m_tot / (A_total*rhow) #draft [m]
+draft = m_tot / (A_total*rhow) #draft [m]    # draft [m]
 
 print(f'The draft is {draft} m ')
 
-h_out =  (h_bf +  h_bb) - draft  # height of the floater outside water
+h_out =  (h_bf +  h_bb) - draft              # height of the floater outside water
 
 #%% Determine positions of columns based on centroid. The x axis is pointing
 #   from the front floater to the ones in the back, in the direction of the
@@ -89,26 +87,27 @@ x_3 =  - 2 * edge / 3
 
 #%% Calculate center of mass coordinates
 
-z_CM_tow = (z_hub / 2 + h_out) # z center of mass tower
+z_CM_tow = (z_hub / 2 + h_out)        # z center of mass tower
 
-z_CM_bf = h_out - h_bf / 2   # z center of back floater
-z_CM_ff = h_out - h_bf / 2   # z center of front floater
+z_CM_bf = h_out - h_bf / 2            # z center of back floater
+z_CM_ff = h_out - h_bf / 2            # z center of front floater
 
-z_CM_bb = (h_bb / 2) - draft   # z center of back ballast
-z_CM_fb = (h_fb / 2) - draft   # z center of front ballast
+z_CM_bb = (h_bb / 2) - draft          # z center of back ballast
+z_CM_fb = (h_fb / 2) - draft          # z center of front ballast
 
+# z position of center of mass
 z_CM_tot = (2 * z_CM_tow * mt + 2 * (z_hub + h_out) * (mr + mn) + 2 * z_CM_bf * m_bf +\
             2 * z_CM_bb * m_bb + z_CM_ff * m_ff + z_CM_fb * m_fb) / m_tot
 
 print(f'The center of mass is at {z_CM_tot} m')
 
-# center of mass around y
+# y position center of mass
 x_CM_tot = ((mr + mn + mt + m_bf + m_bb) * x_1 + (mr + mn + mt + m_bf + m_bb) * x_2\
             + (m_ff + m_fb) *x_3) / m_tot
 
 print(f'The center of mass around y is at {x_CM_tot} m')
 
-# center of mass around x
+# x position center of mass
 y_CM_tot = ((mr + mn + mt + m_bf + m_bb) * y_1 + (mr + mn + mt + m_bf + m_bb) * y_2\
             + (m_ff + m_fb) *y_3) / m_tot
 
@@ -116,6 +115,7 @@ print(f'The center of mass around x is at {y_CM_tot} m')
 
 #%% Calculate center of bouyancy coordinates
 
+# z position of center of buoyancy
 z_CB_tot = - draft / 2
 
 print(f'The center of bouyancy is at {z_CB_tot} m')
@@ -124,11 +124,13 @@ Vol_1_sub = draft * A_back
 Vol_2_sub = draft * A_back
 Vol_3_sub = draft * A_front
 
+# x position of center of mass
 x_CB_tot = (Vol_1_sub * x_1 + Vol_2_sub * x_2 + Vol_3_sub * x_3) /\
                     (Vol_1_sub + Vol_2_sub + Vol_3_sub)
                     
 print(f'The center of bouyancy around y is at {x_CB_tot} m')
-                    
+      
+# y position of center of mass              
 y_CB_tot = (Vol_1_sub * y_1 + Vol_2_sub * y_2 + Vol_3_sub * y_3) /\
                     (Vol_1_sub + Vol_2_sub + Vol_3_sub)
 
@@ -136,36 +138,34 @@ print(f'The center of bouyancy around x is at {y_CB_tot} m')
 
 #%% Hydrostatic restoring matrix
 # surge
-C_11 = 0
-C_12 = 0
-C_13 = 0
-C_14= 0
+C_11 = 0       # surge-surge
+C_12 = 0       # surge-heave
+C_13 = 0       # surge-pitch
+C_14= 0        # surge-yaw
 
 # heave
-C_21 = 0
-C_22 = 2 * rhow * g * A_back + rhow * g * A_front
+C_21 = 0       # heave-surge
+C_22 = 2 * rhow * g * A_back + rhow * g * A_front   # heave-heave
 
 I_x = rhow * g * (2 * A_back * x_1 +  A_front * x_3)
-C_23 = I_x
-
-C_24 = 0
+C_23 = I_x     # heave-pitch
+C_24 = 0       # heave-yaw
 
 # pitch
-C_31 = 0
-C_32 = C_23
+C_31 = 0       # pitch-surge
+C_32 = C_23    # pitch-heave
 
 I_xx = rhow * g * (2 * np.pi * D_b**4 / 64 + np.pi * D_f**4 / 64 +\
                     2 * A_back * x_1**2 +\
                         A_front * x_3**2)
-C_33 = m_tot * g * (z_CB_tot - z_CM_tot) + I_xx
-
-C_34 = 0
-
+C_33 = m_tot * g * (z_CB_tot - z_CM_tot) + I_xx   # pitch-pitch
+C_34 = 0       # pitch-yaw
+  
 # yaw
-C_41 = 0
-C_42 = 0
-C_43 = - m_tot * g * (y_CB_tot - y_CM_tot)
-C_44 = 0
+C_41 = 0       # yaw-surge
+C_42 = 0       # yaw-heave
+C_43 = - m_tot * g * (y_CB_tot - y_CM_tot)   # yaw-pitch
+C_44 = 0       # yaw-yaw
 
 
 C_hydro = np.array([[C_11, C_21, C_31, C_41],
